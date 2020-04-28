05b40a337bebb7745dd13c0f9f065fc1e28a9292
Rune Myrskog <rune.myrskog@mail.utoronto.ca>
2020-04-28 14:59:46 -0400

    added tkinter popup

--- Path_Find_Visualizer.py
+++ Path_Find_Visualizer.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
-import pygame
 import AStar
 import threading
+import pygame
+import tkinter as tk
+from tkinter import Label, Entry, Button, messagebox
 
 # color constants
+
 OPEN_COLOR = (0, 255, 0)
 CLOSED_COLOR = (255, 0, 0)
 PATH_COLOR = (0, 0, 0)
@@ -27,12 +30,24 @@ BLOCK_HEIGHT = (SCREEN_HEIGHT - BLOCK_BUFFER * (ROWS + 1)) / ROWS
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-screen.fill(BACKGROUND_COLOR)
 
 grid = [[AStar.UNBLOCKED for _ in range(ROWS)] for _ in range(COLS)]
-start_pos = (10, 10)
-end_pos = (70, 50)
+start_pos = (10, 10) # default start position
+end_pos = (70, 50) # default end position
 updated = []
+
+
+class AStarThread(threading.Thread):
+    """
+    Thread to run AStar algorithm on. After completing task, stores resulting
+    shortest path that it found in path attribute for use in UI thread
+    """
+
+    path: [(int, int)]
+
+    def run(self):
+        self.path = AStar.a_star_search(grid, start_pos, end_pos, delay=DELAY)
+        return
 
 
 def is_endpoint(pos) -> bool:
@@ -122,17 +137,64 @@ def update_grid():
                     update_block((i, j), OPEN_COLOR)
 
 
-class AStarThread(threading.Thread):
-    """
-    Thread to run AStar algorithm on. After completing task, stores resulting
-    shortest path that it found in path attribute for use in UI thread
-    """
+def assign_positions(window, start_entry, end_entry):
 
-    path: [(int, int)]
+    for entry in {start_entry, end_entry}:
+        if not ((entry[0].isdigit() and entry[1].isdigit())
+                and (-1 < int(entry[0]) < COLS and -1 < int(entry[1]) < ROWS)):
+            messagebox.showwarning(title="invalid entry",
+                                   message="please enter valid positions")
+            return
 
-    def run(self):
-        self.path = AStar.a_star_search(grid, start_pos, end_pos, delay=DELAY)
-        return
+    global start_pos
+    global end_pos
+    start_pos = int(start_entry[0]), int(start_entry[1])
+    end_pos = int(end_entry[0]), int(end_entry[1])
+    window.destroy()
+
+
+def popup():
+
+    window = tk.Tk()
+    start_x_label = Label(window, text="Start(x) (0 - {0}): ".format(COLS-1))
+    start_x_box = Entry(window)
+    start_x_label.grid(row=1, column=1)
+    start_x_box.grid(row=1, column=2)
+    start_x_box.insert(0, str(start_pos[0]))
+
+    start_y_label = Label(window, text="Start(y) (0 - {0}):".format(ROWS-1))
+    start_y_box = Entry(window)
+    start_y_label.grid(row=2, column=1)
+    start_y_box.grid(row=2, column=2)
+    start_y_box.insert(0, str(start_pos[1]))
+
+    end_x_label = Label(window, text="End(x) (0 - {0}): ".format(COLS-1))
+    end_x_box = Entry(window)
+    end_x_label.grid(row=1, column=3)
+    end_x_box.grid(row=1, column=4)
+    end_x_box.insert(0, str(end_pos[0]))
+
+    end_y_label = Label(window, text="End(y) (0 - {0}):".format(ROWS-1))
+    end_y_box = Entry(window)
+    end_y_label.grid(row=2, column=3)
+    end_y_box.grid(row=2, column=4)
+    end_y_box.insert(0, str(end_pos[1]))
+
+    obstacles_label = Label(window,
+                            text="click/drag squares to create obstacles",
+                            fg="green")
+    obstacles_label.grid(row=3, column=2, columnspan=4)
+    restart_label = Label(window, text="Press R to restart", fg="green")
+    restart_label.grid(row=4, column=2, columnspan=4)
+    start_label = Label(window,
+                        text="Press SPACE to start path finding algorithm",
+                        fg="green")
+    start_label.grid(row=5, column=2, columnspan=4)
+
+    submit = Button(window, text='Go', command=lambda: assign_positions(window, (start_x_box.get(), start_y_box.get()), (end_x_box.get(), end_y_box.get())))
+    submit.grid(row=6, column=1, columnspan=4)
+
+    window.mainloop()
 
 
 def run_visualizer():
@@ -146,6 +208,8 @@ def run_visualizer():
     running = True
     a_star_thread = None
 
+    reset_display()
+    popup()
     reset_display()
 
     while running:
@@ -166,11 +230,11 @@ def run_visualizer():
 
                 if event.key == pygame.K_r:
                     can_edit = True
+                    popup()
                     reset_display()
 
         if pressed and can_edit:
             x, y = get_grid_coord(pygame.mouse.get_pos())
-
             if not is_endpoint((x, y)):
                 grid[x][y] = 0
                 update_block((x, y), BLOCKED_COLOR)
